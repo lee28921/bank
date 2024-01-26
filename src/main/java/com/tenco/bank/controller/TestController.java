6d1f8f7c78fe5acfc2226f1348c6895f38c7df0c
dlwlals6790 <dlwlals6790@gmail.com>
2024-01-26 14:04:52 +0900

    exception bandler 기본 코드 완료

--- src/main/java/com/tenco/bank/controller/TestController.java
+++ src/main/java/com/tenco/bank/controller/TestController.java
@@ -1,8 +1,12 @@
 package com.tenco.bank.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.tenco.bank.handler.exception.CustomPageException;
+import com.tenco.bank.handler.exception.CustomRestfulException;
 
 @Controller
 @RequestMapping("/test1") // 대문
@@ -11,7 +15,7 @@ public class TestController {
 	// 주소 설계
 	// http://localhost:80/test1/main
 	@GetMapping("/main")
-	public String mainPage() {
+	public void mainPage() {
 		System.out.println("hello bank!");
 		// 인증 검사
 		// 유효성 검사
@@ -20,6 +24,9 @@ public class TestController {
 		// prefix : /WEB-INF/view
 		// suffix : .jsp
 		
-		return "/layout/main";
+		// 예외 발생
+		throw new CustomPageException("페이지가 존재하지 않습니다.", HttpStatus.NOT_FOUND);
+		
+		//return "/layout/main";
 	}
 }
